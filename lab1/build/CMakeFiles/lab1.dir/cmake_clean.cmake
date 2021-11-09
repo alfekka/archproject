44f145ac7804f7062f8b7553ca14407e0a0be755
alfekka <rvanova.lyu@phystech.edu>
2021-11-09 18:13:03 +0300

    builded

---
+++ lab1/build/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/config/config.cpp.o"
+  "CMakeFiles/lab1.dir/database/database.cpp.o"
+  "CMakeFiles/lab1.dir/database/person.cpp.o"
+  "CMakeFiles/lab1.dir/main.cpp.o"
+  "lab1"
+  "lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
